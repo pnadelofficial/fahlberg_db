3b6b7426cb0fb70951c887be014b16f1dceab90e
peter nadel <peter.m.nadel@gmail.com>
2024-09-13 13:06:53 -0400

    added empty string defaults

--- sql_utils.py
+++ sql_utils.py
@@ -10,10 +10,7 @@ class DatabaseManager:
     def insert(self, table, **kwargs):
         columns = ', '.join(kwargs.keys())
         placeholders = ', '.join(['?'] * len(kwargs))
-        values = tuple(kwargs.values())
-        print(values)
-        for v in values:
-            print(v, type(v))
+        values = tuple([v[0] if isinstance(v, tuple) else v for v in kwargs.values()])
         query = f"INSERT INTO {table} ({columns}) VALUES ({placeholders})"
         self.cur.execute(query, values)
         self.conn.commit()
